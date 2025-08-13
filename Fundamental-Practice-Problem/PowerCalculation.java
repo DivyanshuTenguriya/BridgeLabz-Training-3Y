ba47abc40eb186fa997e025d4c003d36ff233054
DivyanshuTenguriya <divyanshutenguriya@gmail.com>
2025-08-13 16:51:26 +0530

    ASSIGNMENT DONE

---
+++ Fundamental-Practice-Problem/PowerCalculation.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PowerCalculation {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int base=scn.nextInt();
+        int exponent=scn.nextInt();
+        double result=Math.pow( exponent, base);
+        System.out.println(result);
+    }
+}
